cf09de0070a64297b58e240bcf2ed9192f08c145
bolaleka <bhadmuslekan@gmail.com>
2023-04-24 13:10:27 -0700

    Export to JSON file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,21 +7,12 @@ import requests
 import sys
 import json
 
-# Check that an employee ID has been provided as a command-line argument
-if len(sys.argv) != 2:
-    print("Usage: {} EMPLOYEE_ID".format(sys.argv[0]))
-    sys.exit(1)
-
 # Retrieve the employee's TODO list
 employee_id = sys.argv[1]
 response = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
                         .format(employee_id))
 todos = response.json()
 user_id = todos[0].get("userId")
-
-# Count the number of completed tasks and the total number of tasks
-num_completed_tasks = sum(1 for todo in todos if todo['completed'])
-total_num_tasks = len(todos)
 
 # Print the employee TODO list progress in the required format
 url_user = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
